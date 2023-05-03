ccb5d8eb4d23fa8767565fe46db5ef6c84c179c4
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-03 09:01:23 +0530

    Feat: User,order,cart,products,stripe services added.

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const mongo = require("./shared/mongo");
+
 
 //routes modules imports
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/users.routes");
+const productRoutes = require("./routes/products.routes");
+const cartRoutes = require("./routes/cart.routes");
+const orderRoutes = require("./routes/orders.routes");
+const stripeRoutes = require("./routes/stripe.routes");
 
 (async () => {
     try {
         app.use(cors());
         app.use(express.json());
+
+        await mongo.connect(); //call the mongo connect() for run db
 
         const port = process.env.PORT || 3001;
         // verfication in browser
@@ -17,6 +27,17 @@ const app = express();
         });
 
         //middlewares
+        app.use("/auth", authRoutes);
+
+        app.use("/users", userRoutes);
+
+        app.use("/product", productRoutes);
+
+        app.use("/cart", cartRoutes);
+
+        app.use("/order", orderRoutes);
+
+        app.use("/checkout", stripeRoutes);
 
         //server starter
         app.listen(port, () => {
